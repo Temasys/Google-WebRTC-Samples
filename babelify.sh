210c4f0d2ba0971e2713a12a8dc70363e622670d
Jacques-Olivier Haché <jo.hache@temasys.io>
2019-10-15 16:47:39 +0800

    Created a script porting the samples to es5, compatible with IE 11. Relies on an AJS that needs to be copied into src/js for now. Will eventually be a link to AJS once released

---
+++ babelify.sh
@@ -0,0 +1,26 @@
+#/bin/bash
+
+ES6Dir="src/content"
+ES5Dir="src/content-es5"
+
+if [ -d "$ES5Dir" ]; then
+  rm -rf $ES5Dir
+fi
+cp -r $ES6Dir $ES5Dir
+
+HTML_FILES=`find $ES5Dir -type f \( -iname "*index.html" \)`
+for file in $HTML_FILES
+do
+  echo $file
+  sed -i '' 's,src="https://webrtc.github.io/adapter/adapter-latest.js",src="../../../js/adapter.js",g' $file
+done
+
+JS_FILES=`find $ES5Dir -type f \( -iname "*.js" \)`
+for file in $JS_FILES
+do
+  echo $file
+  # NOTE : I know that getusermedia/getdisplaymedia and datachannel/messaging
+  # fail because they have url based imports. Let's ignore those for now
+  browserify $file -o $file -t [ babelify ]
+done
+
